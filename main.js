2c26ba62b23c68b2354946c803864eda6488f9c0
Nior21 <lhall@ya.ru>
2021-04-08 22:00:14 +0300

    Добавлен код создания блока html кода функцией createPlayer

--- main.js
+++ main.js
@@ -17,3 +17,34 @@ const player2 = {
         console.log(name + ' Fight...')
     }
 }
+
+function createPlayer() {
+    const $player1 = document.createElement('div');
+    $player1.classList.add('player1');
+
+    const $progressbar = document.createElement('div');
+    $progressbar.classList.add('progressbar');
+
+    const $character = document.createElement('div');
+    $character.classList.add('character');
+
+    $player1.appendChild($progressbar);
+    $player1.appendChild($character);
+
+    const $life = document.createElement('div');
+    $life.classList.add('life')
+
+    const $name = document.createElement('div');
+    $name.classList.add('name');
+    $name.innerText = 'Liu Kang';
+
+    $progressbar.appendChild($life);
+    $progressbar.appendChild($name);
+
+    const $img = document.createElement('img');
+    $img.src ='http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
+
+    $character.appendChild($img);
+
+    console.log($player1)
+}
