8848783fe96d2dbfda5827e54ccb916a3f4b21a6
Nior21 <lhall@ya.ru>
2021-04-11 14:37:57 +0300

    В случае победы добавляется "name + win!" вместо lose. Однако отображение блока не работает как надо.

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-const $arenas = document.querySelector('.arenas');
-const $randomButton = document.querySelector('.button');
+const $arenas = document.querySelector ( '.arenas' );
+const $randomButton = document.querySelector ( '.button' );
 
 const player1 = {
     player: 1,
     name: 'SCORPION',
-    hp: 50,
+    hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['weapon_1'],
     attack: function () {
-        console.log(name + ' Fight...')
+        console.log ( name + ' Fight...' )
     }
 }
 const player2 = {
     player: 2,
     name: 'SUB-ZERO',
-    hp: 80,
+    hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ['weapon_2'],
     attack: function () {
-        console.log(name + ' Fight...')
+        console.log ( name + ' Fight...' )
     }
 }
 
 
 function createElement(tag, className) {
-    const $tag = document.createElement(tag);
+    const $tag = document.createElement ( tag );
 
     if (className) {
-        $tag.classList.add(className);
+        $tag.classList.add ( className );
     }
 
     return $tag;
@@ -36,51 +36,67 @@ function createElement(tag, className) {
 
 function createPlayer(character) {
 
-    const $player = createElement('div', 'player' + character.player);
-    const $progressbar = createElement('div', 'progressbar');
-    const $character = createElement('div', 'character');
-    const $life = createElement('div', 'life');
-    const $name = createElement('div', 'name');
-    const $img = createElement('img');
+    const $player = createElement ( 'div', 'player' + character.player );
+    const $progressbar = createElement ( 'div', 'progressbar' );
+    const $character = createElement ( 'div', 'character' );
+    const $life = createElement ( 'div', 'life' );
+    const $name = createElement ( 'div', 'name' );
+    const $img = createElement ( 'img' );
 
-    $player.appendChild($progressbar);
-    $player.appendChild($character);
+    $player.appendChild ( $progressbar );
+    $player.appendChild ( $character );
     $life.style.width = character.hp + `%`;
     $name.innerText = character.name;
-    $progressbar.appendChild($life);
-    $progressbar.appendChild($name);
+    $progressbar.appendChild ( $life );
+    $progressbar.appendChild ( $name );
     $img.src = character.img;
-    $character.appendChild($img);
+    $character.appendChild ( $img );
 
     return $player;
 }
 
 
 function changeHP(character) {
-
-    const $playerLife = document.querySelector('.player' + character.player + ' .life');
-    character.hp -= 20;
-    $playerLife.style.width = character.hp + '%';
+    const $playerLife = document.querySelector ( '.player' + character.player + ' .life' );
+    character.hp -= Math.ceil(Math.random() * 20); // случайное число от 1 до 20
 
     if (character.hp < 0) {
-        $arenas.appendChild(playerLose(character.name));
+        character.hp = 0;
+        if (character.player == 2) {
+            playerWin(player1.name);
+        } else if (character.player == 1) {
+            playerWin(player2.name);
+        }
     }
+
+    $playerLife.style.width = character.hp + '%'
+    console.log ( `#### player ${character.player} ${character.name} character.hp:`, character.hp );
 }
 
 
 function playerLose(name) {
-    const $loseTitle = createElement('div', 'loseTitle');
-    $loseTitle.innerText = name + ' lose';
+    const $loseTitle = createElement ( 'div', 'loseTitle' );
+    $loseTitle.innerText = name + ' lose!';
 
-    return $loseTitle
+    return $loseTitle;
 }
 
 
-$randomButton.addEventListener('click', function () {
-    changeHP(player1);
-    changeHP(player2);
-})
+function playerWin(name) {
+    const $winTitle = createElement ( 'div', 'winTitle' );
+    $winTitle.innerText = name + ' wins!';
+
+    $randomButton.disabled = true // отключаем кнопку
+
+    return $arenas.appendChild ( $winTitle )
+}
 
 
-$arenas.appendChild(createPlayer(player1));
-$arenas.appendChild(createPlayer(player2));
+$randomButton.addEventListener ( 'click', function () {
+    changeHP ( player1 );
+    changeHP ( player2 );
+} )
+
+
+$arenas.appendChild ( createPlayer ( player1 ) );
+$arenas.appendChild ( createPlayer ( player2 ) );
